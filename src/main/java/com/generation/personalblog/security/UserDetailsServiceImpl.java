9cb87c4798fea82e067f737a2e4961ecb243ef12
lukas-95 <lucrecmen@gmail.com>
2023-05-26 17:08:56 -0300

    Add security layer

---
+++ src/main/java/com/generation/personalblog/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.generation.personalblog.security;
+
+import com.generation.personalblog.model.Usuario;
+import com.generation.personalblog.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<Usuario> usuario= usuarioRepository.findByUsuario(userName);
+
+        if (usuario.isPresent())
+            return new UserDetailsImpl(usuario.get());
+        else
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+
+    }
+}
